e5417adcf88d0385174872decbc9053fbc140178
Sebastian Córdova <scordova@stanalytics.es>
2024-02-20 20:42:34 +0100

    Añadidas más funcionalidades

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 
-    
+from langchain.prompts import PromptTemplate
+from langchain import HuggingFaceHub
+from langchain.chains import LLMChain, SequentialChain
 import streamlit as st
 import os
 from dotenv import load_dotenv
@@ -7,31 +9,41 @@ load_dotenv()
 
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv('huggingface_token')
 
-from langchain.prompts import PromptTemplate
-from langchain import HuggingFaceHub
-from langchain.chains import LLMChain
-
-@st.cache_resource
-def load_llm():
-    return HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature": 1.0, "max_length": 512})
-
-llm = load_llm()
 
 st.title('🦜️🔗YouTube GPT Creator')
-
 prompt = st.text_input('Plug in your prompt here')
 
+# Prompt templates
 title_templete = PromptTemplate(
     input_variables=['topic'],
     template = 'write me a youtube video title about {topic}'
 )
 
-title_chain = LLMChain(llm=llm, prompt = title_templete, verbose=True)
+script_templete = PromptTemplate(
+    input_variables=['title'],
+    template = 'write me a youtube video script base on this title: {title}'
+)
+
+# Model
+@st.cache_resource
+def load_llm():
+    return HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature": 1.0, "max_length": 512})
+
+# LLms
+llm = load_llm()
+title_chain = LLMChain(llm=llm, prompt = title_templete, verbose=True, output_key='title')
+script_chain = LLMChain(llm=llm, prompt = script_templete, verbose=True, output_key='script')
+ #sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'], 
+ #                                  output_variables=['script'], verbose=True)
 
 
 if prompt:
-    response = title_chain.run(topic=prompt)
-    st.write(response)
+    title_result = title_chain.run(prompt)
+    st.write("Título:", title_result)  # Directamente escribe el resultado sin usar ['output']
+
+    # Ejecuta la cadena para el guion usando el mismo prompt del usuario
+    script_result = script_chain.run(title_result)
+    st.write("Guion:", script_result) 
 
 
 
