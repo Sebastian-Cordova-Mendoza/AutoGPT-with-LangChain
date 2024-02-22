745b13fc6fe787f9946c52d9f870791c59078271
Sebastian Córdova <scordova@stanalytics.es>
2024-02-22 15:06:24 +0100

    Añadida info de wikipedia desde utilities

--- appOpenIA.py
+++ appOpenIA.py
@@ -1,11 +1,15 @@
 
-from langchain_community.llms import OpenAI
-from langchain.chains import LLMChain, SimpleSequentialChain, SequentialChain
-from langchain.prompts import PromptTemplate
 import streamlit as st
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
+from langchain_community.llms import OpenAI
+from langchain.chains import LLMChain, SimpleSequentialChain, SequentialChain
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
+
 
 os.environ["OPENAI_API_KEY"] = os.getenv('openaikey')
 
@@ -18,19 +22,37 @@ title_template = PromptTemplate(
 )
 
 script_template = PromptTemplate(
-    input_variables=['title'],
-    template = 'write me a poem base on this title: {title}'
+    input_variables=['title', 'wikipedia_research'],
+    template = 'write me a poem of two verses base on this title: {title} while leveraging this wikipedia reserch:{wikipedia_research}'
 )
 
+
+title_memory = ConversationBufferMemory(input_key = 'topic', momery_key='chat_history')
+script_memory = ConversationBufferMemory(input_key = 'title', momery_key='chat_history')
+
 llm = OpenAI(temperature=0.9)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True,
+                        output_key = 'title', memory=title_memory)
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, 
+                        output_key = 'script',memory=script_memory)
+#sequentialchain = SequentialChain(chains=[title_chain, script_chain], input_variables = ['topic'],
+#                                       output_variables=['title','script'], verbose=True)
 
-
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key = 'title')
-script_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key = 'script')
-sequentialchain = SimpleSequentialChain(chains=[title_chain, script_chain], input_variables = ['topic'],
-                                        output_variables=['title','script'], verbose=True)
+wiki = WikipediaAPIWrapper()
 
 if prompt:
-    response = sequentialchain.run({'topic':prompt})
-    st.write(response['title'])
-    st.write(response['script'])
+    title=title_chain.run(prompt)
+    wiki_research = wiki.run(prompt)
+    script=script_chain.run(title=title, wikipedia_research=wiki_research)
+    st.write(title)
+    st.write(script)
+
+
+    with st.expander('Title History'):
+        st.info(title_memory.buffer)
+
+    with st.expander('Scirpt History'):
+        st.info(script_memory.buffer)
+
+    with st.expander('wikipedia_research'):
+        st.info(wiki_research)
